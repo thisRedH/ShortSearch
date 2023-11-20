03c87f41d7e150c7389b2ff39232b04bb2fff11b
thisRedH <thisRedH@users.noreply.github.com>
2023-11-20 16:41:55 +0100

    Changed Indentation

--- shortsearch/scripts/background/service_worker.js
+++ shortsearch/scripts/background/service_worker.js
@@ -2,32 +2,31 @@
 importScripts("../common/misc.js");
 
 chrome.commands.onCommand.addListener(async (command, tab) => {
-    if (command.startsWith("search_selected_txt")) {
-        // Send message to content script
-        try {
-            const engineNumber = LAST_NUMBER.exec(command)[1];
-            const enginesList = ((await chrome.storage.sync.get("enginesList")).enginesList);
-            var engine = enginesList[engineNumber];
+if (command.startsWith("search_selected_txt")) {
+    // Send message to content script
+    try {
+        const engineNumber = LAST_NUMBER.exec(command)[1];
+        const enginesList = ((await chrome.storage.sync.get("enginesList")).enginesList);
+        var engine = enginesList[engineNumber];
 
-            if (!engine) {
-                showWarn(`Engine ${engineNumber} does not exist! Using fallback engine 0`);
-                engine = enginesList[0];
-            }
-
-            if (!engine) {
-                showError("Search engine could not be loaded. Using google.com instead");
-                engine = {"name": "Google", "url": "http://google.com/search?q=%s"};
-            }
-
-            await chrome.tabs.sendMessage(tab.id, {
-                event: "search_sel",
-                engineURL: engine.url,
-                urlAutoEval: "loose",       //TODO: load from save + implement in options
-                windowTarget: "_blank",     //TODO: load from save + implement in options
-                engineURLPlaceholder: "%s"  //TODO: load from save + implement in options
-            });
-        } catch(err) {
-            showError(err.message);
+        if (!engine) {
+            showWarn(`Engine ${engineNumber} does not exist! Using fallback engine 0`);
+            engine = enginesList[0];
         }
+
+        if (!engine) {
+            showError("Search engine could not be loaded. Using google.com instead");
+            engine = {"name": "Google", "url": "http://google.com/search?q=%s"};
+        }
+
+        await chrome.tabs.sendMessage(tab.id, {
+            event: "search_sel",
+            engineURL: engine.url,
+            urlAutoEval: "loose",       //TODO: load from save + implement in options
+            windowTarget: "_blank",     //TODO: load from save + implement in options
+            engineURLPlaceholder: "%s"  //TODO: load from save + implement in options
+        });
+    } catch(err) {
+        showError(err.message);
     }
-});
+}});
